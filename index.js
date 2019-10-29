1f4ed211aa6304bb9767a601482b21f93fd64c5b
panming <panming@baidu.com>
2019-10-29 10:31:15 +0800

    init commit

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 'use strict';
 
-function WebpackInlineModernSourcePlugin (htmlWebpackPlugin) {
+import path, { resolve } from 'path';
+import slash  from 'slash';
+import sourceMapUrl  from 'source-map-url';
+import escapeRegex  from 'escape-string-regexp';
+
+export default class WebpackInlineModernSourcePlugin {
+    constructor(webpackPlugin) {
+        this.webpackPlugin = webpackPlugin;
+    }
+
+    apply(compiler) {
+        compiler.hooks.compilation.tap('webpack-inline-modern-source-plugin', compilation => {
+            this.webpackPlugin
+                .getHooks(compilation)
+                .alterAssetTagGroups.tapPromise('webpack-inline-modern-source-plugin', pluginData => {
+                    return new Promise((resolve, reject) => {
+                        const options = pluginData.plugin.options || {};
+                        const regex = options.inlineSource
+    
+                        regex && resolve(this.run(compilation, regex, pluginData));
+                        resolve(pluginData);
+                    });
+                });
+        });
+    }
+
+    run(compilation, regex, pluginData) {
+
+    }
+
+    singleTask() {
+
+    }
+
 
 }
-
-module.exports = WebpackInlineModernSourcePlugin;
